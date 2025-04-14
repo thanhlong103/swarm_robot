bc9f732f235f017643fcac852125744ed67644a4
Nguyen Le Na <leenanguyen204@gmail.com>
2025-04-14 19:53:27 +0700

    fix map

--- simulation_ws/src/turtlebot3_navigation2/launch/navigation2.launch.py
+++ simulation_ws/src/turtlebot3_navigation2/launch/navigation2.launch.py
@@ -34,7 +34,7 @@ def generate_launch_description():
         default=os.path.join(
             get_package_share_directory('tlbot_navigation2'),
             'map',
-            'run.yaml'))
+            'simumap.yaml'))
 
     param_file_name = TURTLEBOT3_MODEL + '.yaml'
     param_dir = LaunchConfiguration(
